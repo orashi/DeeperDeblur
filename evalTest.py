2323f81adef423cf62881f8cbf587aa07baeeb0f
orashi <yuanzheng.ci@gmail.com>
2017-08-29 17:16:00 +0800

    bug fix

--- evalTest.py
+++ evalTest.py
@@ -29,9 +29,10 @@ print(netG)
 if opt.cuda:
     netG.cuda()
 
+os.makedirs(os.path.join(os.path.split(opt.dataroot)[0], os.path.split(opt.dataroot)[1] + ' result'))
 for input, name in dataloader_test:
     prediction = netG(Variable(input.cuda(), volatile=True))
-    vutils.save_image(prediction[2].mul(0.5).add(0.5),
-                      os.path.join(os.path.split(opt.dataroot)[0], os.path.split(opt.dataroot)[1] + ' result',
-                                   name[0] + '.png'), padding=0)
+    vutils.save_image(prediction[2].data.mul(0.5).add(0.5),
+                      os.path.join(os.path.split(opt.dataroot)[0], os.path.split(opt.dataroot)[1] + ' result', name[0]),
+                      padding=0)
     print(name + '　処理完了')
