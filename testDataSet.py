12c747501310355801d965d93e72b3614413a407
orashi <yuanzheng.ci@gmail.com>
2017-10-17 14:07:06 +0800

    :sparkles::zap: make test folder name specifiable and introduce cudnn support

--- testDataSet.py
+++ testDataSet.py
@@ -1,5 +1,6 @@
 import argparse
 
+import torch.backends.cudnn as cudnn
 import torchvision.utils as vutils
 from torch.autograd import Variable
 
@@ -10,13 +11,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--netG', default='', help="path to netG (to continue training)")
 parser.add_argument('--dataroot', required=True, help='path to dataset')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
+parser.add_argument('--id', type=str, default=None, help='experiment identification')
 opt = parser.parse_args()
 print(opt)
 
 ####### regular set up
 if torch.cuda.is_available() and not opt.cuda:
     print("WARNING: You have a CUDA device, so you should probably run with --cuda")
-# cudnn.benchmark = True
+cudnn.benchmark = True
 ####### regular set up end
 
 
@@ -37,7 +39,8 @@ except OSError:
 for input, name in dataloader_test:
     prediction = netG(Variable(input.cuda() if opt.cuda else input, volatile=True))
     vutils.save_image(torch.squeeze(prediction.data).mul(0.5).add(0.5),
-                      os.path.join(os.path.split(opt.dataroot)[0], os.path.split(opt.dataroot)[1] + 'result', name[0]),
+                      os.path.join(os.path.split(opt.dataroot)[0], os.path.split(opt.dataroot)[1] + 'result' + opt.id,
+                                   name[0]),
                       padding=0)
     print(name[0] + '　処理完了')
     prediction = 0
